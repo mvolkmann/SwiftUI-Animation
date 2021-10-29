336b9f1c123d7275ac989f59b83703992466b12a
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-29 06:25:42 -0500

    Initial Commit

---
+++ SwiftUI-Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI-Animation
+//
+//  Created by R. Mark Volkmann on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
