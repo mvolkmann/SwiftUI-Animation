13efbc7e41ae00b56d6607668f72382228050a1f
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-29 08:06:26 -0500

    initial version

--- SwiftUI-Animation/ContentView.swift
+++ SwiftUI-Animation/ContentView.swift
@@ -1,16 +1,71 @@
-//
-//  ContentView.swift
-//  SwiftUI-Animation
-//
-//  Created by R. Mark Volkmann on 10/29/21.
-//
-
 import SwiftUI
 
+enum EasingType: String, CaseIterable {
+    case linear
+    case easeIn
+    case easeOut
+    case easeInOut
+    case spring
+}
+
 struct ContentView: View {
+    @State private var borderColor: Color = .red
+    @State private var color = false
+    @State private var easingType: EasingType = .linear
+    @State private var on = false
+    @State private var opacity = false
+    @State private var rotate = false
+    @State private var scale = false
+    
+    private var easingFunction: Animation {
+        switch easingType {
+        case .linear: return Animation.linear(duration: 1)
+        case .easeIn: return Animation.easeIn(duration: 2)
+        case .easeOut: return Animation.easeOut(duration: 2)
+        case .easeInOut: return Animation.easeInOut(duration: 2)
+        case .spring: return Animation.spring(dampingFraction: 0.5).speed(0.3)
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            VStack {
+                Text("First Line")
+                Text("Second Line")
+                Text("🎉").font(.largeTitle)
+            }
             .padding()
+            .border(borderColor, width: 10)
+            .opacity(!opacity || on ? 1 : 0)
+            .scaleEffect(!scale || on ? 1 : 0)
+            .rotationEffect(.degrees(!rotate || on ? 0 : 360))
+            //TODO: Is an implicit animation required to specify the easing function?
+            .animation(easingFunction) // implicit animation
+            
+            
+            NavigationView { // Picker will be disabled without this.
+                Form {
+                    Picker("Easing Function", selection: $easingType) {
+                        ForEach(EasingType.allCases, id: \.self) { easingType in
+                            Text("\(easingType.rawValue)").tag(easingType)
+                        }
+                    }
+                    Toggle("Animate Color?", isOn: $color)
+                    Toggle("Animate Opacity?", isOn: $opacity)
+                    Toggle("Animate Rotation?", isOn: $rotate)
+                    Toggle("Animate Scale?", isOn: $scale)
+                    Button("Toggle") {
+                        withAnimation { // explicit animation
+                            if color {
+                                borderColor = borderColor == .red ? .blue : .red
+                            }
+                            on.toggle()
+                        }
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
